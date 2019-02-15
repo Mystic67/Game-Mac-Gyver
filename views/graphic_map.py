9d16cf3ea84309fddde334dbbf63195bbfa1a56d
Mystic67 <webmaster@passion-geek.com>
2019-02-15 15:01:35 +0100

    add new class Graphic_Map

---
+++ views/graphic_map.py
@@ -0,0 +1,52 @@
+#! /usr/bin/python3
+# -*-coding: utf-8-*-
+
+import pygame
+from pygame.locals import *
+import config.settings as constants
+from models.map import Map
+
+class Graphic_Map(Map):
+    ''' This class inherited from class Map for graphical map drawing'''
+    def __init__(self, filename):
+        Map.__init__(self, filename)
+
+
+    def draw_map(self, window):
+        ''' This method draw the map tile per tile '''
+
+
+        tile_error = pygame.image.load(constants.TILE_ERROR).convert()
+        tiles = pygame.image.load(constants.TILES_PICTURES)
+        tile_start = tiles.subsurface(40,80,40,40).convert_alpha()#Colonne,Ligne,H,W
+        tile_wall = tiles.subsurface(480,440,40,40).convert_alpha()
+        tile_goal = tiles.subsurface(320,40,40,40).convert_alpha()
+        tile_path = tiles.subsurface(320,320,40,40).convert_alpha()
+
+
+        '''Search the positions and values from loaded file with the item
+           generator method defined in parent class Map.
+           Draw the tiles with the positions.'''
+        for position, value in self.items():
+            if value == constants.START:
+                x=position.get_x() * constants.SPRITE_SIZE
+                y=position.get_y() * constants.SPRITE_SIZE
+                window.blit(tile_start, (y,x))
+            elif value == constants.WALL:
+                x=position.get_x() * constants.SPRITE_SIZE
+                y=position.get_y() * constants.SPRITE_SIZE
+                window.blit(tile_wall, (y,x))
+            elif value == constants.PATH:
+                x=position.get_x() * constants.SPRITE_SIZE
+                y=position.get_y() * constants.SPRITE_SIZE
+                window.blit(tile_path, (y,x))
+            elif value == constants.GOAL:
+                x=position.get_x() * constants.SPRITE_SIZE
+                y=position.get_y() * constants.SPRITE_SIZE
+                window.blit(tile_goal, (y,x))
+            else:
+                x=position.get_x() * constants.SPRITE_SIZE
+                y=position.get_y() * constants.SPRITE_SIZE
+                window.blit(tile_error, (y,x))
+                print("Error string in map at position ({},{})".format(position.get_y()+1,\
+                position.get_x()+1))
