9a66ebe433e72ae6771bc0b865c4d200a1e00d36
Mystic67 <webmaster@passion-geek.com>
2019-02-15 14:58:33 +0100

    modified models/position.py

--- models/position.py
+++ models/position.py
@@ -1,41 +1,49 @@
 #! /usr/bin/python3
 # -*-coding: utf-8-*-
-"""
 
-"""
 class Position:
+
+    
     """This class receive in parameter attribut x an y and return
      Position(x,y) """
     def __init__(self,x,y):
-        self._x = x
-        self._y = y
-        self.position = (self._x,self._y) #absisse/ordonee position
+        self.__x = x
+        self.__y = y
+        self.__position = (self.__x,self.__y) #absisse/ordonee position
+
 
     def __repr__(self):
         """method to return position if enter instance in the
         interpreter"""
-        return str(self.position)
+        return str(self.__position)
+
 
     def __str__(self):
         """method to return position if enter print(instance) in the
         interpreter """
         return repr(self)
 
+
     def __hash__(self):
         '''method to convert position to hash value.
         Needed to accès to a dictionary with key '''
-        return hash(self.position)
+        return hash(self.__position)
+
 
     def __eq__(self, pos):
-        return self.position == pos.position
+        return self.__position == pos.position
+
 
     def get_x(self):
-        '''method to read the private variable _x'''
-        return self._x
+        '''method to read the private variable __x'''
+        return self.__x
+
 
     def get_y(self):
-        '''method to read the private variable _y'''
-        return self._y
+        '''method to read the private variable __y'''
+        return self.__y
 
-if __name__=="__main__":
-    Position()
+
+    def get_position(self):
+        '''method to read the private variable __position'''
+        return self.__position
