60bae3d474c86db848be85b4b2155d639a06e588
Mystic67 <webmaster@passion-geek.com>
2019-02-15 14:49:57 +0100

    add a new config directory with new settings.py

---
+++ config/settings.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: Utf-8 -*
+
+# Map constants
+START="S"
+GOAL="G"
+PATH="."
+WALL="M"
+
+MAP="models/map.txt"
+
+
+#Pygame views constants
+#window settings
+WINDOW_TITLE = "Mac Gyver"
+ICON_PICTURE = "images/MacGyver.png"
+
+
+#mase pictures
+HOME_PICTURE = "images/Game_MacGyver.png"
+MAC_GYVER = "images/MacGyver.png"
+MURDOC = "images/Murdoc.png"
+TILES_PICTURES ="images/floor-tiles-40x40.png"
+TILE_ERROR = "images/Error_tile.png"
+
+
+#maze settings
+SPRITE_BY_SIDE = 15
+SPRITE_SIZE = 40
+WINDOW_SIDE_SIZE = SPRITE_BY_SIDE * SPRITE_SIZE
