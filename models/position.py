854e2ae8c1e000d7c552b586d42bae3c834a5488
Mystic67 <webmaster@passion-geek.com>
2019-02-18 15:45:01 +0100

    correct the error from method __eq__() and change the comments.

--- models/position.py
+++ models/position.py
@@ -2,25 +2,26 @@
 # -*-coding: utf-8-*-
 
 class Position:
+    '''This class receive in parameter attribut x an y and return
+     Position(x,y) '''
 
-    
-    """This class receive in parameter attribut x an y and return
-     Position(x,y) """
+
     def __init__(self,x,y):
+        ''' Initialize the attributs'''
         self.__x = x
         self.__y = y
         self.__position = (self.__x,self.__y) #absisse/ordonee position
 
 
     def __repr__(self):
-        """method to return position if enter instance in the
-        interpreter"""
+        '''method to return position if enter instance in the
+        interpreter'''
         return str(self.__position)
 
 
     def __str__(self):
-        """method to return position if enter print(instance) in the
-        interpreter """
+        '''method to return position if enter print(instance) in the
+        interpreter '''
         return repr(self)
 
 
@@ -30,8 +31,10 @@ class Position:
         return hash(self.__position)
 
 
-    def __eq__(self, pos):
-        return self.__position == pos.position
+    def __eq__(self, position):
+        '''This method is needed if want to compare a position with tuple
+        self.__position == (x,y) '''
+        return self.__position == position
 
 
     def get_x(self):
