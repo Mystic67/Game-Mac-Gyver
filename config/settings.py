b1fa5874b65da00c75209026f1e633073fce8644
Mystic67 <webmaster@passion-geek.com>
2019-02-18 15:36:33 +0100

    add constants

--- config/settings.py
+++ config/settings.py
@@ -20,8 +20,13 @@ ICON_PICTURE = "images/MacGyver.png"
 HOME_PICTURE = "images/Game_MacGyver.png"
 MAC_GYVER = "images/MacGyver.png"
 MURDOC = "images/Murdoc.png"
+GADGETS_NAME = ["chalumeau", "ether", "couteau", "tube", "seringue"]
+GADGETS_PICTURES = ["images/chalumeau.png", "images/ether.png", "images/couteau_suisse.png",\
+ "images/tube_plastique.png","images/seringue.png"]
 TILES_PICTURES ="images/floor-tiles-40x40.png"
 TILE_ERROR = "images/Error_tile.png"
+YOU_WIN = "images/You_win.png"
+YOU_LOSE = "images/You_lose.png"
 
 
 #maze settings
