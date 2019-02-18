3e584e815b0c7ce6e3b21722ddfe8c81c4df92cf
Mystic67 <webmaster@passion-geek.com>
2019-02-18 15:49:42 +0100

    add __init_position(), modified attributs and change the comments.

--- views/sprite.py
+++ views/sprite.py
@@ -5,53 +5,83 @@ import pygame
 from pygame.locals import *
 import config.settings as constants
 from views.graphic_map import Graphic_Map
-from models.position import Position as pos
+#from models.position import Position as pos
+import random
 
 
 class Sprite:
-    def __init__(self, instance_map, image, init_pos, move=False ):
-        self.__map =instance_map
+    object_create = 0
+
+    def __init__(self, instance_map, image, init_pos="random", move=False ):
+        ''' Initialize the attributs'''
+        self.__map = instance_map
         self.__image = image
         self.__init_pos = init_pos
         self.__move = move
         self.__move_conditions = [constants.PATH, constants.START, constants.GOAL]
-        for position, value in self.__map.items():
-            if value == self.__init_pos:
-                self.__x= position.get_x()
-                self.__y= position.get_y()
+        self.__init_position()
 
 
-    ''' This method is needed to display the sprite on new position after move.'''
     def display(self, window):
+        ''' This method is needed to display the sprite on at position or refresh
+        at position after move.'''
         sprite = pygame.image.load(self.__image).convert_alpha()
         window.blit(sprite, (self.__y * constants.SPRITE_SIZE,\
         self.__x * constants.SPRITE_SIZE))
 
 
-    ''' This methods move the sprite if sprite movable
-        The spite can move if target position is Path'''
+    def __init_position(self):
+        '''This method initialise the position with random or given position. '''
+        if self.__init_pos == "random":
+            type(self).object_create += 1
+            #print("objet créer {}".format(self.create_object))
+            rand_pos = random.choice([pos for (pos, val) in self.__map.items() if val == constants.PATH])
+            self.__x = rand_pos.get_x()
+            self.__y = rand_pos.get_y()
 
-    '''Method to move_down the sprite '''
+        else:
+            # if the position is given in param. The position (x,y) is stored in list whith list comprehention,
+            # also need to accès with index 0
+            position = [pos for (pos, val) in self.__map.items() if val == self.__init_pos]
+            self.__x = position[0].get_x()
+            self.__y = position[0].get_y()
+
+    def __del__(self):
+        #if self.__init_pos == "random":
+        type(self).object_create -= 1
+
+    def get_position(self):
+        '''Method to get the position from sprite '''
+        return (self.__y, self.__x)
+
+
     def move_down(self):
+        '''Method to move_down the sprite if give attribut True to param __move.
+        The spite can move if target position is Path'''
         if self.__move:
             if self.__map[self.__x+1, self.__y] in self.__move_conditions:
                 self.__x += 1
 
 
-    '''Method to move_up the sprite '''
     def move_up(self):
+        '''Method to move_up the sprite if give attribut True to param __move.
+        The spite can move if target position is Path'''
         if self.__move:
             if self.__map[self.__x-1, self.__y] in self.__move_conditions:
                 self.__x -= 1
 
-    '''Method to move_right the sprite '''
+
     def move_right(self):
+        '''Method to move_right the sprite if give attribut True to param __move.
+        The spite can move if target position is Path'''
         if self.__move:
             if self.__map[self.__x, self.__y+1] in self.__move_conditions:
                 self.__y += 1
 
-    '''Method to move_left the sprite '''
+
     def move_left(self):
+        '''Method to move_left the sprite if give attribut True to param __move.
+        The spite can move if target position is Path'''
         if self.__move:
             if self.__map[self.__x, self.__y-1] in self.__move_conditions:
                 self.__y -= 1
